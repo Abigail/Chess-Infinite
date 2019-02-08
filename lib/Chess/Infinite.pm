048f02ee11296717909c05c979d9cc0aa4ff507e
Abigail <abigail@abigail.be>
2019-02-08 22:56:28 +0100

    Added Dragon King and Dragon Horse.
    The promoted Rook and Bishop in Shogi.

--- lib/Chess/Infinite.pm
+++ lib/Chess/Infinite.pm
@@ -28,7 +28,8 @@ use Chess::Infinite::Board::Spiral;
 my @CHESS       = qw [King Queen Rook Bishop Knight];
 my @LEAPERS     = qw [Knight Ferz Alfil Tripper Camel Zebra Wazir
                       Dabbaba Threeleaper];
-my @SHOGI       = qw [King Rook Bishop GoldGeneral SilverGeneral Lance];
+my @SHOGI       = qw [King Rook DragonKing Bishop DragonHorse GoldGeneral
+                      SilverGeneral Lance];
 my @LARGE_SHOGI = qw [DrunkenElephant];
 
 my @PIECES  = do {my %seen; grep {!$seen {$_} ++}
