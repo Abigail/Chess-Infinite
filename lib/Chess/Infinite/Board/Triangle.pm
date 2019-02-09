3fb6cc30e89f2d7e8f9851d25aede8f07ccfd58f
Abigail <abigail@abigail.be>
2019-02-09 22:04:38 +0100

    Add block comments to each subroutine of Board::Triangle.

--- lib/Chess/Infinite/Board/Triangle.pm
+++ lib/Chess/Infinite/Board/Triangle.pm
@@ -17,19 +17,50 @@ our @ISA = qw [Chess::Infinite::Board];
 use List::Util qw [max];
 use POSIX      qw [ceil];
 
+
+################################################################################
 #
-# Given some coordinates, return the corresponding value.
+# to_value ($self, $x, $y)
 #
+# Given coordinates ($x, $y), return the corresponding value.
+#
+#    IN:  - $self: Current object
+#         - $x:    X-coordinate, 0-based
+#         - $y:    Y-coordinate, 0-based
+#
+#   OUT:  Value on position ($x, $y)
+#
+#   PRE:  $x >= 0 && $y >= 0
+#
+# TESTS:  100-Board/110-Triangle/100-to_value.t
+#
+################################################################################
+
 sub to_value ($self, $x, $y) {
     my $z = ($x + $y) + 1;
     ($z * $z + $z) / 2 - $x;
 }
 
 
+################################################################################
+#
+# to_coordinates ($self, $value)
+#
+# Given a value, return the coordinates the value is on.
+#
+#    IN:  - $self:  Current object
+#         - $value: Value we want coordinates for
+#
+#   OUT:  Position; in list context this is returned as a two element list;
+#         in scalar context, this is returned as a reference to a two
+#         element array.
+#
+#   PRE:  $value > 0
+#
+# TESTS:  100-Board/110-Triangle/100-to_value.t
+#
+################################################################################
 
-#
-# Given a value, return the corresponing coordinates.
-#
 sub to_coordinates ($self, $value) {
     #
     # The 'inverse' of the next triangle number
@@ -43,9 +74,25 @@ sub to_coordinates ($self, $value) {
     wantarray ? ($x, $y) : [$x, $y];
 }
 
+################################################################################
+#
+# is_valid ($self, $x, $y)
+#
+# Given a set of coordinates ($x, $y), return whether it is "on the board".
+# For our board, this means neither of the coordinates my be less than 0.
+#
+#    IN:  - $self: Current object
+#         - $x:    X-coordinate, 0-based
+#         - $y:    Y-coordinate, 0-based
+#
+#   OUT:  True if the coordinates are on the board, false otherwise.
+#
+# TESTS:  100-Board/110-Triangle/110-is_valid.t
+#
+################################################################################
 
 sub is_valid ($self, $x, $y) {
-    $x >= 0 && $y >= 0;
+    return $x >= 0 && $y >= 0;
 }
 
 
